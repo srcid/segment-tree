b09a085285ee857c8f17d6a208645f5c51bcc6b6
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-04-18 06:50:39 -0300

    chore(main): formatted

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from rich import print
-from segment_tree.segment_tree import SegmentTree
 
+from segment_tree.interval import Interval
+from segment_tree.segment_tree import SegmentTree
 
 #      0, 1, 2, 3, 4, 5, 6
 arr = [1, 3, 5, 7, 9, 11]
 sgt = SegmentTree(arr)
 
+print(f'{arr}')
 print(sgt.asTree())
